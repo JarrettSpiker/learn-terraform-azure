50d1d66adbedefc10982eee5ba03e230742fdae3
Jarrett Spiker <jarrettspiker@gmail.com>
2022-11-17 18:29:57 -0500

    add more vms

--- main.tf
+++ main.tf
@@ -117,6 +117,321 @@ resource "azurerm_virtual_machine" "vm" {
 
 }
 
+resource "azurerm_virtual_machine" "vm2" {
+  name                  = "${var.prefix}TFVM2"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+  tags                  = var.tags
+
+  storage_os_disk {
+    name              = "${var.prefix}OsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = lookup(var.sku, var.location)
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "${var.prefix}TFVM2"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+}
+
+
+resource "azurerm_virtual_machine" "vm3" {
+  name                  = "${var.prefix}TFVM3"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+  tags                  = var.tags
+
+  storage_os_disk {
+    name              = "${var.prefix}OsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = lookup(var.sku, var.location)
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "${var.prefix}TFVM3"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+}
+
+
+resource "azurerm_virtual_machine" "vm4" {
+  name                  = "${var.prefix}TFVM4"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+  tags                  = var.tags
+
+  storage_os_disk {
+    name              = "${var.prefix}OsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = lookup(var.sku, var.location)
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "${var.prefix}TFVM4"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+}
+
+
+resource "azurerm_virtual_machine" "vm5" {
+  name                  = "${var.prefix}TFVM5"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+  tags                  = var.tags
+
+  storage_os_disk {
+    name              = "${var.prefix}OsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = lookup(var.sku, var.location)
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "${var.prefix}TFVM5"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+}
+
+
+resource "azurerm_virtual_machine" "vm6" {
+  name                  = "${var.prefix}TFVM6"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+  tags                  = var.tags
+
+  storage_os_disk {
+    name              = "${var.prefix}OsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = lookup(var.sku, var.location)
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "${var.prefix}TFVM6"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+}
+
+
+resource "azurerm_virtual_machine" "vm7" {
+  name                  = "${var.prefix}TFVM7"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+  tags                  = var.tags
+
+  storage_os_disk {
+    name              = "${var.prefix}OsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = lookup(var.sku, var.location)
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "${var.prefix}TFVM7"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+}
+
+
+resource "azurerm_virtual_machine" "vm8" {
+  name                  = "${var.prefix}TFVM8"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+  tags                  = var.tags
+
+  storage_os_disk {
+    name              = "${var.prefix}OsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = lookup(var.sku, var.location)
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "${var.prefix}TFVM8"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+}
+
+
+resource "azurerm_virtual_machine" "vm9" {
+  name                  = "${var.prefix}TFVM9"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+  tags                  = var.tags
+
+  storage_os_disk {
+    name              = "${var.prefix}OsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = lookup(var.sku, var.location)
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "${var.prefix}TFVM9"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+}
+
+
+resource "azurerm_virtual_machine" "vm10" {
+  name                  = "${var.prefix}TFVM10"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS1_v2"
+  tags                  = var.tags
+
+  storage_os_disk {
+    name              = "${var.prefix}OsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = lookup(var.sku, var.location)
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "${var.prefix}TFVM10"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+}
+
+
 data "azurerm_public_ip" "ip" {
   name                = azurerm_public_ip.publicip.name
   resource_group_name = azurerm_virtual_machine.vm.resource_group_name
